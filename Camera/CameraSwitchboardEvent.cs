9fdca4b43efbac40c0ab3ac5ff65c926671427aa
jplebre <jplebre@gmail.com>
2014-04-10 13:11:25 +0100

    Added CameraSwitchboardEvent
    This can be added to any script in order to change the camera. Please
    read the comments and copy paste (UNTESTED!)

---
+++ Camera/CameraSwitchboardEvent.cs
@@ -0,0 +1,18 @@
+﻿/* Based of Unity 4.x Cookbook script "CameraSwitch"
+ * Copy paste this code to the event's script.
+ * 
+ */
+
+
+
+/*
+ * This is to trigger camera changes through events
+ * untested 10/Apr/2014
+//Add these variable to the top of the script
+//They will show up on the inspector
+public string switchboardObject;
+public string switchToCamera;
+
+//Copy/Paste this line into the function that is triggering events.
+GameObject.Find(switchboardObject).GetComponent("CameraSwitchboard").SwitchCamera(switchToCamera);
+*/	
